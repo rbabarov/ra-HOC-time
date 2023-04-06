24721bd504b5139c39200d5a68eb2d3cbd8510a3
rbabarov <90253870+rbabarov@users.noreply.github.com>
2023-04-06 16:12:42 +0500

    Initial commit

---
+++ src/db/fakeData.js
@@ -0,0 +1,35 @@
+
+const videoList = [
+  {
+    name: "name1",
+    url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+    date: "2021-07-31 13:24:00",
+  },
+  {
+    name: "name2",
+    url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+    date: "2022-03-03 12:10:00",
+  },
+  {
+    name: "name3",
+    url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
+    date: "2018-02-03 23:16:00",
+  },
+  {
+    name: "name4",
+    url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
+    date: "2022-10-03 12:10:00",
+  },
+  {
+    name: "name5",
+    url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
+    date: "2018-01-01 16:17:00",
+  },
+  {
+    name: "name6",
+    url: "https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0",
+    date: "2022-10-17 12:24:00",
+  },
+];
+
+export default videoList;
